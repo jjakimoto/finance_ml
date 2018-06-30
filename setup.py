e35155b8a9b6c9f83a1be6a085011191b4e773c3
jjakimoto <f.j.akimoto@gmail.com>
2018-06-30 07:52:02 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="finance_ml",
+    version='0.1',
+    description='utility library for finance',
+    author='jjakimoto',
+    author_email='f.j.akimoto@gmail.com',
+    packages=['finance_ml']
+)
